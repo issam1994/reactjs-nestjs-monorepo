e677469b71288d1fa66ca38db68772e221fdf9dc
Issam <issamAitOuahmane@gmail.com>
2025-12-20 22:40:39 +0100

    update(auth): refactored to make jwt auth guard global to reduce repetition and created a Public decorator to use it for public endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
-import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { AuthService } from './auth.service';
 import { UsersService } from 'src/users/users.service';
+import { Public } from './decorators/public.decorator';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,19 +19,24 @@ export class AuthController {
     private usersService: UsersService,
   ) {}
 
+  @Public()
+  @Post('register')
+  register(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Request() req) {
     return this.authService.login(req.user);
   }
 
-  @UseGuards(LocalAuthGuard)
   @Post('logout')
   logout(@Request() req) {
     return req.logout();
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req: any) {
     const userId: number = req.user.id;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Apply JWT guard globally
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
+  //
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
